a2153a2244956fdee9bc86cf39b612a5c52c6d00
Ashutosh Batra <ashutoshbatra.ab1997@gmail.com>
2019-05-23 11:09:38 +0530

    Add further part of project

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Cache;
 use App\User;
 
 class ProfilesController extends Controller
@@ -16,10 +17,31 @@ class ProfilesController extends Controller
     public function index(User $user)
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
+
+        $postCount = Cache::remember(
+            'count.posts' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user) {
+                return $user->posts->count();
+            });
+
+        $followersCount = Cache::remember(
+            'count.followers' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user){
+                return $user->profile->followers->count();
+            });
+
+        $followingCount = Cache::remember(
+            'count.following' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user){
+                return $user->following->count();
+            });
         //dd($follows); 
     	// dd($user);
     	//$user = User::findOrFail($user);
-        return view('profiles.index', compact('user','follows'));
+        return view('profiles.index', compact('user','follows','postCount','followersCount','followingCount'));
     }
 
     public function edit(User $user)
